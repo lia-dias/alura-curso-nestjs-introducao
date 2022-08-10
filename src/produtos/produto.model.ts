b4c622655db7068b371f078e4b8025200ae339f3
Lia Dias <liadias914@gmail.com>
2022-08-10 19:08:43 -0300

    (model) Adicionamos o model de produto para ser manipulado pelo controller

---
+++ src/produtos/produto.model.ts
@@ -0,0 +1,4 @@
+export default class Produto {
+    public id: number;
+    constructor(public codigo: string, public nome: string, public preco: number){}
+}
--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,30 +1,42 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { of } from "rxjs";
+import Produto from './produto.model';
 
 @Controller('produtos')
 export default class ProdutosController {
 
+    private produtosDB: Produto[] = [
+        new Produto("LV1", "Algoritmos: Teoria e Prática", 220.00),
+        new Produto("LV2", "Arquitetura de Software", 69.90),
+        new Produto("LV3", "Problemas da Ciência da Computação em Python", 59.90),
+    ];
+
     @Get()
-    obterTodos(): string{
-        return 'Lista todos os produtos'; 
+    obterTodos(): Produto[]{
+        return this.produtosDB.map((produto, index) => (!!produto ? {id: index, ...produto}: null)).filter( produto => !!produto); 
     };
 
     @Get(':id')
-    obterProdutoEspecifico(@Param('id') id: number | string): string {
-        return `Produto específico: ${id}`;
+    obterProdutoEspecifico(@Param('id') id: number): Produto {
+        return {id, ...this.produtosDB[id]};
     }
 
     @Post()
-    criarProduto(@Body() produto): string {
-        return `Produto Criado: ${JSON.stringify(produto)}`; 
+    criarProduto(@Body() produto: Produto): Produto {
+        const id = this.produtosDB.push(produto)-1;
+        return {id, ...this.produtosDB[id]}; 
     }
 
     @Put(':id')
-    atualizarProduto(@Body() produto, @Param('id') id: number | string): string {
-        return `Produto ${id} atualizado ${JSON.stringify(produto)}`; 
+    atualizarProduto(@Body() produto, @Param('id') id: number): Produto{
+        this.produtosDB[id] = produto;
+        return {id, ...this.produtosDB[id]};
     }
 
     @Delete(':id')
-    apagarProduto(@Param('id') id: number | string): string {
-        return `Produto ${id} apagado`; 
+    apagarProduto(@Param('id') id: number): Produto {
+        const produto = this.produtosDB[id];
+        this.produtosDB[id] = null;
+        return produto;
     }
 }
